0e96f5c46c580a320bffc2a7d056be48453ce080
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-21 18:43:53 +0200

    added minor changes

--- calculator.py
+++ calculator.py
@@ -27,12 +27,15 @@ operation = {
     "*" : mult,
     "/" : div,
 }
-
+# enter first number
 num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 for symbol in operation:
     print(symbol)
+# enter operator
 operation_symbol = input("Enter any one of the above symbols: ")
+# enter second number
+num2 = int(input("Enter second number: "))
+
 calculation_function = operation[operation_symbol]  # variable is assigned to a function
 answer = calculation_function(num1, num2)    # now we pass parameters into that function
 
