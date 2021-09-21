95d193bc335b8e7e931adcfcd9c0a5285362d9c1
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-21 23:40:30 +0200

    added feature

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 # day 10 project
 
+continue_calc = False
+
 # add function
 def add(num1, num2):
     '''Returns sum of two numbers'''
@@ -40,3 +42,15 @@ calculation_function = operation[operation_symbol]  # variable is assigned to a 
 answer = calculation_function(num1, num2)    # now we pass parameters into that function
 
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+if input("Continue operation ('y' or 'n')? ") == "y":
+    operation_symbol = input("Enter any one of the above symbols: ")
+    num3 = int(input("Enter next number: "))
+    calc_func = operation[operation_symbol]
+    answer2 = calc_func(answer, num3)
+    print(f"{answer} {operation_symbol} {num3} = {answer2}")
+
+else:
+    print("Goodbye! Shutting down....")
+
+'''ADD WHILE LOOP & RECURSION'''
