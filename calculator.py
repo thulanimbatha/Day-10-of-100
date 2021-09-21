23fb9dcf49aaa95bc200deb3e19c81df53097d6a
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-21 18:42:01 +0200

    finished project

--- calculator.py
+++ calculator.py
@@ -1,2 +1,39 @@
 # day 10 project
 
+# add function
+def add(num1, num2):
+    '''Returns sum of two numbers'''
+    return num1 + num2
+
+# subtraction function
+def subtract(num1, num2):
+    '''Returns subtraction of two numbers'''
+    return num1 - num2
+
+# multiplication function
+def mult(num1, num2):
+    '''Returns product of two numbers'''
+    return num1 * num2
+
+# division function
+def div(num1, num2):
+    '''Returns division of two numbers'''
+    return num1 / num2
+
+# dictionary
+operation = {
+    "+" : add,
+    "-" : subtract,
+    "*" : mult,
+    "/" : div,
+}
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+for symbol in operation:
+    print(symbol)
+operation_symbol = input("Enter any one of the above symbols: ")
+calculation_function = operation[operation_symbol]  # variable is assigned to a function
+answer = calculation_function(num1, num2)    # now we pass parameters into that function
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
