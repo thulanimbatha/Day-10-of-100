3c45d4d1cb0e05fa0cb786f7d368e5c3e01eebf5
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-21 23:58:18 +0200

    added feature 2.0

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # day 10 project
 
-continue_calc = False
+exit_calc = False
 
 # add function
 def add(num1, num2):
@@ -30,27 +30,24 @@ operation = {
     "/" : div,
 }
 # enter first number
-num1 = int(input("Enter first number: "))
+num1 = float(input("Enter first number: "))
 for symbol in operation:
     print(symbol)
-# enter operator
-operation_symbol = input("Enter any one of the above symbols: ")
-# enter second number
-num2 = int(input("Enter second number: "))
 
-calculation_function = operation[operation_symbol]  # variable is assigned to a function
-answer = calculation_function(num1, num2)    # now we pass parameters into that function
+while not exit_calc:
+    # enter operator
+    operation_symbol = input("Enter an operation: ")
+    # enter second number
+    num2 = float(input("Enter next number: "))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    calculation_function = operation[operation_symbol]  # variable is assigned to a function
+    answer = calculation_function(num1, num2)    # now we pass parameters into that function
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-if input("Continue operation ('y' or 'n')? ") == "y":
-    operation_symbol = input("Enter any one of the above symbols: ")
-    num3 = int(input("Enter next number: "))
-    calc_func = operation[operation_symbol]
-    answer2 = calc_func(answer, num3)
-    print(f"{answer} {operation_symbol} {num3} = {answer2}")
-
-else:
-    print("Goodbye! Shutting down....")
+    if input("Continue operation ('y' or 'n')? ") == "y":
+        num1 = answer
+    else:
+        print("Goodbye! Shutting down....")
+        exit_calc = True
 
 '''ADD WHILE LOOP & RECURSION'''
