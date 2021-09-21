f41bea60b8248c32c76cab160c011758d9506bd1
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-22 00:06:29 +0200

    added feature 3.0

--- calculator.py
+++ calculator.py
@@ -1,7 +1,5 @@
 # day 10 project
 
-exit_calc = False
-
 # add function
 def add(num1, num2):
     '''Returns sum of two numbers'''
@@ -29,25 +27,32 @@ operation = {
     "*" : mult,
     "/" : div,
 }
-# enter first number
-num1 = float(input("Enter first number: "))
-for symbol in operation:
-    print(symbol)
 
-while not exit_calc:
-    # enter operator
-    operation_symbol = input("Enter an operation: ")
-    # enter second number
-    num2 = float(input("Enter next number: "))
+def calculator():
+    # enter first number
+    num1 = float(input("Enter first number: "))
+    for symbol in operation:
+        print(symbol)
+        
+    exit_calc = False
 
-    calculation_function = operation[operation_symbol]  # variable is assigned to a function
-    answer = calculation_function(num1, num2)    # now we pass parameters into that function
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    while not exit_calc:
+        # enter operator
+        operation_symbol = input("Enter an operation: ")
+        # enter second number
+        num2 = float(input("Enter next number: "))
 
-    if input("Continue operation ('y' or 'n')? ") == "y":
-        num1 = answer
-    else:
-        print("Goodbye! Shutting down....")
-        exit_calc = True
+        calculation_function = operation[operation_symbol]  # variable is assigned to a function
+        answer = calculation_function(num1, num2)    # now we pass parameters into that function
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-'''ADD WHILE LOOP & RECURSION'''
+        if input("Continue operation ('y') or start a new calculation ('n')? ") == "y":
+            num1 = answer
+        else:
+            print("Goodbye! Shutting down....")
+            exit_calc = True
+            calculator()    # recursion
+
+calculator()
+
+
